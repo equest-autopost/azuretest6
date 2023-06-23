9a9dd2a1e49bb630262e716a042dff9d0e6ee9d6
equest-autopost <james.nelson@equest.com>
2023-06-23 10:38:39 -0700

    test

--- aspnet-core/src/azuretest6.EntityFrameworkCore/Migrations/20230623172222_test.Designer.cs
+++ aspnet-core/src/azuretest6.EntityFrameworkCore/Migrations/20230623172222_test.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using azuretest6.EntityFrameworkCore;
 
@@ -11,9 +12,10 @@ using azuretest6.EntityFrameworkCore;
 namespace azuretest6.Migrations
 {
     [DbContext(typeof(azuretest6DbContext))]
-    partial class azuretest6DbContextModelSnapshot : ModelSnapshot
+    [Migration("20230623172222_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
